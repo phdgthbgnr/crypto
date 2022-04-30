f7d44db5374bfeccd376c2ec7d835ea129af9f0e
linux_samsung <linux@samsung>
2022-04-30 22:40:45 +0200

    update sql request (join films / paths)

--- _inc/index.php
+++ _inc/index.php
@@ -3,7 +3,15 @@ require './class/connect.php';
 $conn = new connect();
 
 
-$res = $conn->execute_query("SELECT id_film FROM $conn->films LIMIT 5");
+$res = $conn->execute_query(
+  "SELECT tb1.*, 
+  tb1.filepathid, 
+  tb2.* 
+  FROM 
+  $conn->films as tb1 
+  JOIN $conn->path as tb2 
+  ON tb2.id_path = tb1.filepathid  LIMIT 5"
+);
 if ($res) {
   while ($row = mysqli_fetch_assoc($res)) {
     print_r($row);
