03b13e2a61a3c363bcd9681212ee268afb75dd4c
linux_samsung <linux@samsung>
2022-05-01 20:32:27 +0200

    fix Posts in module TODO : test getPosts

--- src/_js/app.js
+++ src/_js/app.js
@@ -1,12 +1,16 @@
 'use strict';
 import Alpine from 'alpinejs';
-import Posts from './com/posts';
+import { Posts } from './com/posts';
+import getPosts from './com/getPosts';
 import '../_css/style.scss';
 
 Alpine.data('Posts', Posts);
 
 // Alpine.magic('json', () => {
-//   return posts;
+//   console.log(Posts);
+//   return Posts;
 // });
+
+console.log(Posts());
 
 Alpine.start();
--- src/_js/com/posts.js
+++ src/_js/com/posts.js
@@ -1,65 +1,32 @@
-// export default () => ({
-//   Posts() {
-//     return async () => {
-//       try {
-//         let res = await axios({
-//           url: 'hhttp://localhost:8081',
-//           method: 'get',
-//           timeout: 8000,
-//           headers: {
-//             'Content-Type': 'application/json',
-//           },
-//         });
-
-//         if (res.status == 200) {
-//           // test for status you want, etc
-//           console.log(res.status);
-//         }
-//         // Don't forget to return something
-//         console.log(res);
-//         return res.data;
-//       } catch (err) {
-//         console.error(err);
-//       }
-//     };
-//   },
-// });
 // https://postsrc.com/posts/how-to-make-ajax-request-in-alpine-js
+// https://www.alpinetoolbox.com/
+// https://codepen.io/eddieebeling/pen/dyoZOBX
 'use strict';
 import axios from 'axios';
 
-// export default () => ({
-//   Posts() {
-//     return [
-//       { id: 1, filename: 'toto1' },
-//       { id: 2, filename: 'toto2' },
-//     ];
-//   },
-// });
+const getPosts = () => {
+  return {
+    getPOsts: async () => {
+      const res = await axios({
+        url: 'http://localhost:8081',
+        method: 'get',
+        timeout: 8000,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      return res.data.payload;
+    },
+  };
+};
 
-export default () => ({
-  async Posts() {
-    const response = await axios({
-      url: 'http://localhost:8081',
-      method: 'get',
-      timeout: 8000,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    console.table(response.data.payload);
-    return response.data.payload;
-  },
-});
+const Posts = () => {
+  return {
+    Posts: () => [
+      { id: 1, filename: 'toto1' },
+      { id: 2, filename: 'toto2' },
+    ],
+  };
+};
 
-// axios
-//   .get('http://localhost:8081')
-//   .then(function (response) {
-//     // handle success
-//     // console.table(response.data.payload);
-//     return response.data.payload;
-//   })
-//   .catch(function (error) {
-//     // handle error
-//     console.log(error);
-//   });
+export { Posts, getPosts };
