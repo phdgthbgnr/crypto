dfad08c640a0ba4ed01288cd3638d33d5fda07b7
linux_samsung <linux@samsung>
2022-05-01 13:18:57 +0200

    add axios request

--- src/_js/com/posts.js
+++ src/_js/com/posts.js
@@ -2,13 +2,21 @@ import axios from 'axios';
 
 export default () => ({
   Posts() {
-    return [
-      { title: 'title 1' },
-      { title: 'title 2' },
-      { title: 'title 3' },
-      { title: 'title 4' },
-      { title: 'title 5' },
-      { title: 'title 6' },
-    ];
+    // return [
+    //   { title: 'title 1' },
+    //   { title: 'title 2' },
+    //   { title: 'title 3' },
+    //   { title: 'title 4' },
+    //   { title: 'title 5' },
+    //   { title: 'title 6' },
+    // ];
+    return axios
+      .get('http://localhost:8081')
+      .then((response) => {
+        console.log(response.data); // assign it to items in the store
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   },
 });
