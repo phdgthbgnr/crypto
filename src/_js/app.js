8f5c0a002ed791a97f69f3e0879abdf75b8a92c9
linux_samsung <linux@samsung>
2022-05-01 18:14:22 +0200

    remove comments / console

--- src/_js/app.js
+++ src/_js/app.js
@@ -3,17 +3,6 @@ import Alpine from 'alpinejs';
 import Posts from './com/posts';
 import '../_css/style.scss';
 
-// const posts = [
-//   { title: 'title 1' },
-//   { title: 'title 2' },
-//   { title: 'title 3' },
-//   { title: 'title 4' },
-//   { title: 'title 5' },
-//   { title: 'title 6' },
-// ];
-
-console.log(Posts());
-
 Alpine.data('Posts', Posts);
 
 // Alpine.magic('json', () => {
