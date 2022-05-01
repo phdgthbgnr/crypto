0d406ede8aacfca018dee0fbb7106e4e6d461141
linux_samsung <linux@samsung>
2022-05-01 12:15:36 +0200

    update response add headers

--- _inc/index.php
+++ _inc/index.php
@@ -1,7 +1,18 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+
 require './class/connect.php';
+
 $conn = new connect();
 
+$response = array(
+  'status' => -1,
+  'error' => 'sql error',
+  'payload' => []
+);
+
+$data = array();
 
 $res = $conn->execute_query(
   "SELECT tb1.*, 
@@ -12,8 +23,26 @@ $res = $conn->execute_query(
   JOIN $conn->path as tb2 
   ON tb2.id_path = tb1.filepathid  LIMIT 5"
 );
+
 if ($res) {
-  while ($row = mysqli_fetch_assoc($res)) {
-    print_r($row);
+  while ($row = mysqli_fetch_object($res)) {
+    $d = array(
+      'id' => $row->id_film,
+      'filename' => $row->filename,
+      'imagedata' => $row->imgencrypt,
+      'width' => $row->width,
+      'height' => $row->height,
+      'size' => $row->filesize,
+      'creationdate' => $row->creationdate,
+      'accessdate' => $row->accessdate
+    );
+    array_push($data, $d);
   }
+  $response['status'] = 1;
+  $response['error'] = '';
+  $response['payload'] = $data;
 }
+
+echo json_encode($response);
+
+exit;
