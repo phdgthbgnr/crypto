0f8180fe74b0ccc2c67e4d83d69d3c7aac87f2f5
linux_samsung <linux@samsung>
2022-05-01 18:14:58 +0200

    update axios request

--- src/_js/com/posts.js
+++ src/_js/com/posts.js
@@ -1,49 +1,65 @@
+// export default () => ({
+//   Posts() {
+//     return async () => {
+//       try {
+//         let res = await axios({
+//           url: 'hhttp://localhost:8081',
+//           method: 'get',
+//           timeout: 8000,
+//           headers: {
+//             'Content-Type': 'application/json',
+//           },
+//         });
+
+//         if (res.status == 200) {
+//           // test for status you want, etc
+//           console.log(res.status);
+//         }
+//         // Don't forget to return something
+//         console.log(res);
+//         return res.data;
+//       } catch (err) {
+//         console.error(err);
+//       }
+//     };
+//   },
+// });
+// https://postsrc.com/posts/how-to-make-ajax-request-in-alpine-js
+'use strict';
+import axios from 'axios';
+
+// export default () => ({
+//   Posts() {
+//     return [
+//       { id: 1, filename: 'toto1' },
+//       { id: 2, filename: 'toto2' },
+//     ];
+//   },
+// });
+
 export default () => ({
-  Posts() {
-    return [
-      { title: 'title 1' },
-      { title: 'title 2' },
-      { title: 'title 3' },
-      { title: 'title 4' },
-      { title: 'title 5' },
-      { title: 'title 6' },
-    ];
+  async Posts() {
+    const response = await axios({
+      url: 'http://localhost:8081',
+      method: 'get',
+      timeout: 8000,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    console.table(response.data.payload);
+    return response.data.payload;
   },
 });
 
-// let t = async function () {
-//   try {
-//     const resp = await axios.get('http://localhost:8081');
-//     console.error(resp.data);
-//     return resp.data;
-//   } catch (err) {
-//     // Handle Error Here
-//     console.error(err);
-//   }
-// };
-// return t;
-//},
-
-// Posts() {
-// return async () => {
-//   try {
-//     let res = await axios({
-//       url: 'hhttp://localhost:8081',
-//       method: 'get',
-//       timeout: 8000,
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//     });
-//     if (res.status == 200) {
-//       // test for status you want, etc
-//       console.log(res.status);
-//     }
-//     // Don't forget to return something
-//     return res.data;
-//   } catch (err) {
-//     console.error(err);
-//   }
-// };
-// },
-//}
+// axios
+//   .get('http://localhost:8081')
+//   .then(function (response) {
+//     // handle success
+//     // console.table(response.data.payload);
+//     return response.data.payload;
+//   })
+//   .catch(function (error) {
+//     // handle error
+//     console.log(error);
+//   });
