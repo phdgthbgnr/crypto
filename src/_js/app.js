be07edf1533e7ccf7006159d4f98a6646daa5286
linux_samsung <linux@samsung>
2022-05-08 23:13:35 +0200

    remove import getPosts

--- src/_js/app.js
+++ src/_js/app.js
@@ -1,7 +1,6 @@
 'use strict';
 import Alpine from 'alpinejs';
 import { Posts } from './com/posts';
-import { getPosts } from './com/getPosts';
 import '../_css/style.scss';
 
 Alpine.data('Posts', Posts);
