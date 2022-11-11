aeae97ff21ba84070a4e8ca38bf2b76cd71c471c
user01 <user01@wsl_P50>
2022-11-11 23:13:42 +0100

    add worker in decryptFromWorker

--- src/_js/com/decryptFromWorker.js
+++ src/_js/com/decryptFromWorker.js
@@ -1,12 +1,17 @@
-const decryptFromWorker = (element) => {
-  console.log(element);
-  const worker = new Worker('worker.js', { type: 'module' });
-  // worker.onmessage = getDecrypt();
-  worker.postMessage([
-    { cipher: element.filename, type: 'filename', id: element.id },
-    { cipher: element.imagedata, type: 'image', id: element.id },
-    { cipher: element.path, type: 'path', id: element.id },
-  ]);
+const decryptFromWorker = (datas) => {
+  let result = [];
+  datas.forEach((e) => {
+    const worker = new Worker('worker.js', { type: 'module' });
+    worker.onmessage = (el) => {
+      console.log('from worker ', el.data);
+    };
+    worker.postMessage([
+      { cipher: e.filename, type: 'filename', id: e.id },
+      { cipher: e.imagedata, type: 'image', id: e.id },
+      { cipher: e.path, type: 'path', id: e.id },
+    ]);
+  });
+  return result;
 };
 
 export { decryptFromWorker };
--- src/_js/com/posts.js
+++ src/_js/com/posts.js
@@ -38,18 +38,8 @@ const Posts = () => ({
             'Content-Type': 'application/json',
           },
         });
-        res.data.payload.forEach((element) => {
-          // console.log(element);
-          decryptFromWorker(element);
-          // const worker = new Worker('worker.js', { type: 'module' });
-          // worker.onmessage = decryptFromWorker;
-          // worker.postMessage([
-          //   { cipher: element.filename, type: 'filename', id: element.id },
-          //   { cipher: element.imagedata, type: 'image', id: element.id },
-          //   { cipher: element.path, type: 'path', id: element.id },
-          // ]);
-        });
-        this.datas = res.data;
+        // console.log(res.data.payload);
+        decryptFromWorker(res.data.payload);
       } catch (e) {
         // failure
       } finally {
