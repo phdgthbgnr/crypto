250e26ee14d250f22bed881428ec48629962c03e
user01 <user01@wsl_P50>
2022-11-21 22:28:06 +0100

    fix comp loadVideo

--- src/_js/com/loadVideo.js
+++ src/_js/com/loadVideo.js
@@ -1,12 +1,13 @@
 export default () => ({
   path: '',
   videosRoot: 'http://192.168.1.11/films/',
-  loadvideo() {
-    this.path = this.videosRoot + this.data.path + this.data.filename;
-  },
+  // loadvideo() {
+  //   this.path = this.videosRoot + this.data.path + this.data.filename;
+  // },
   init() {
     this.path = this.videosRoot + this.data.path + this.data.filename;
     // this.$watch('path', (value) => {
+    //   console.log('change ', value);
     // });
   },
   get getPath() {
