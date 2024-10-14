589c2a2afdc00c3a6f3ba672ed28e9167e44b0be
user01 <user01@wsl_P50>
2024-10-14 18:01:01 +0200

    add header

--- _inc/index.php
+++ _inc/index.php
@@ -1,5 +1,6 @@
 <?php
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT');
 header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
 
 require './class/connect.php';
